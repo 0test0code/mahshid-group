888037ea428fedc258f506263792468349cfe644
mirkamranmahshid <secure.communicatione@gmail.com>
2025-03-07 09:18:27 +0330

    firstCommitCloudflare

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-const createNextIntlPlugin = require('next-intl/plugin');
- 
+//const createNextIntlPlugin = require('next-intl/plugin');
+import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
